cbf9f13292ce3b9ee1dc6078538e97ee048db149
Mstislav <mstislavd@gmail.com>
2024-04-24 17:28:14 +0300

    add normalizeURL function and tests

--- crawl.js
+++ crawl.js
@@ -1,5 +1,17 @@
 function normalizeURL(url){
-    return url
+    normalizedURL = url
+    
+    if (url.slice(0,7) === "http://"){
+        normalizedURL = normalizedURL.slice(7)
+    } else if (url.slice(0,8) === "https://"){
+        normalizedURL = normalizedURL.slice(8)
+    }
+
+    if (normalizedURL.slice(-1) === "/"){
+        normalizedURL = normalizedURL.slice(0,-1)
+    }
+
+    return normalizedURL
 }
 
 module.exports = {
--- crawl.test.js
+++ crawl.test.js
@@ -3,5 +3,8 @@ const { test, expect } = require('@jest/globals')
 const { normalizeURL } = require('./crawl.js')
 
 test('normalize URLs', () => {
-    expect(normalizeURL("url")).toBe("url2");
+    expect(normalizeURL("http://blog.boot.dev/path")).toBe("blog.boot.dev/path");
+    expect(normalizeURL("https://blog.boot.dev/path")).toBe("blog.boot.dev/path");
+    expect(normalizeURL("http://blog.boot.dev/path/")).toBe("blog.boot.dev/path");
+    expect(normalizeURL("https://blog.boot.dev/path/")).toBe("blog.boot.dev/path");
   });
