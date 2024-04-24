c51aa28d1edba2c8307dc59eb02aaf314bbf82e5
Mstislav <mstislavd@gmail.com>
2024-04-24 17:13:46 +0300

    small fix

--- crawl.test.js
+++ crawl.test.js
@@ -2,6 +2,6 @@ const { test, expect } = require('@jest/globals')
 
 const { normalizeURL } = require('./crawl.js')
 
-test('adds 1 + 2 to equal 3', () => {
+test('normalize URLs', () => {
     expect(normalizeURL("url")).toBe("url2");
   });
